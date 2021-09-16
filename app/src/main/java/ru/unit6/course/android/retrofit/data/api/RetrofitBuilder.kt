a6375925bec78348b6ab71e8d7e6866b6e80739f
Aleksandr Agafonov <alex.2exe2@gmail.com>
2021-09-16 17:36:35 +0500

    update host

--- app/src/main/java/ru/unit6/course/android/retrofit/data/api/RetrofitBuilder.kt
+++ app/src/main/java/ru/unit6/course/android/retrofit/data/api/RetrofitBuilder.kt
@@ -2,6 +2,6 @@ package ru.unit6.course.android.retrofit.data.api
 
 object RetrofitBuilder {
 
-    private const val BASE_URL = "https://5e510330f2c0d300147c034c.mockapi.io/"
+    private const val BASE_URL = "https://61433962c8700e00178d01c4.mockapi.io/"
 
 }
